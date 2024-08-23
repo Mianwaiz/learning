974c32ccb7d6b5f792341c96d045a96dad3758f2
waiz python developer <148198099+Mianwaiz@users.noreply.github.com>
2024-08-23 20:30:21 +0500

    Update books.py

--- books.py
+++ books.py
@@ -2,9 +2,14 @@ import scrapy
 
 
 class MySpider(scrapy.Spider):
+
     name = "myspider"
-    username='waiz'
-    password=123
-    def __init__(self, category=None,username=username,password=password, *args, **kwargs):
+    username = 'waiz'
+    password = 123
+
+    def __init__(self, category=None,
+                 username=username, password=password, *args, **kwargs):
         super(MySpider, self).__init__(*args, **kwargs)
-        self.start_urls = [f"http://www.example.com/categories/{category}{username}{password}"]
+        self.start_urls = [
+            f"http://www.example.com/categories/{category}{username}{password}"
+            ]
